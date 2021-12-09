b9e7c3a44df8628d35ac48a8a80273c4261ba659
James Harrison <somtochukwujames2000@gmail.com>
2021-12-09 20:59:44 +0100

    HomePage Component

--- src/Components/HomePageComponent/HomePage.js
+++ src/Components/HomePageComponent/HomePage.js
@@ -1,8 +1,22 @@
+import RelaxImage from "../Assets/Relax-img.svg";
+import './HomePage.css'
 const Homepage = () => { 
-    return (
-        <div className="homepage">
 
+    return (
+      <div className="homepage">
+        <div className="home-brand container">
+          <div className="home-text">
+            <h1>Throw paperwork into the trash where it belongs.</h1>
+            <p>
+              Eliminate all the hassle involved in managing people operations by
+              automating it.
+            </p>
+          </div>
+          <div className="home-image">
+            <img src={RelaxImage} alt="relaximage" />
+          </div>
         </div>
+      </div>
     );
 }
 export default Homepage;
