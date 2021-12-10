6a814c4ae841563a6aa9fa5c06157e9b83ecd3e1
James Harrison <somtochukwujames2000@gmail.com>
2021-12-10 10:58:19 +0100

    Human Resource Section - Desktop view

--- src/Components/HumanResourceComponent/HumanResource.js
+++ src/Components/HumanResourceComponent/HumanResource.js
@@ -1,8 +1,36 @@
+import Frame1 from '../Assets/Frame1.png';
+import Frame2 from '../Assets/Frame2.png';
+import Frame3 from '../Assets/Frame3.png';
+import LastFrame from '../Assets/LastBigFrame.png';
+import './HumanResource.css';
+
 const HumanResource = () => {
     return (
-        <div className="human-resource">
-
+      <div className="human-resource">
+        <div className="container">
+          <div className="humanresource-text">
+            <h2>Human Resources</h2>
+            <p>
+              Onboard new employees, manage the employee lifecycle and measure
+              employee performance.
+            </p>
+          </div>
+          <div className="humanresource-card">
+            <div className="card">
+              <img src={Frame1} />
+            </div>
+            <div className="card">
+              <img src={Frame2} />
+            </div>
+            <div className="card">
+              <img src={Frame3} />
+            </div>
+            </div>
+         <div className='Employeeboard'>
+            <img src={ LastFrame} />
         </div>
+        </div>
+      </div>
     );
 }
 export default HumanResource;
